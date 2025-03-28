9cac582aac0948fa712f770423ed433f5fc978f6
きくたたくや <gtk.9223@gmail.com>
2025-03-28 18:24:45 +0900

    一旦プッシュ

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,15 @@
 import { useState } from "react"
 
-const Form = function(){
-  const [ city, setCity ] = useState<string>("")
+const Form = () => {
+  const [city, setCity] = useState<string>("")
   return (
     <form>
-      <input type="text" name="city" placeholder="都市名" onChange={ function(e){ setCity(e.target.value )}} />
+      <input 
+        type="text" 
+        name="city" 
+        placeholder="都市名" 
+        onChange={(e) => setCity(e.target.value)} 
+      />
       {city}
       <button type="submit">Get Weather</button>
     </form>
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,4 +1,4 @@
-const Title = function(){
+const Title = () => {
   return(
     <h1>React World Weather</h1>
   )
